407898d8fbbf4d2c138450d080a952b2bd788809
ArnaudBan <abanvillet@studio-goliath.com>
2015-05-28 22:01:24 +0200

    Refactoring the flickr API Call

--- flickr-call.class.php
+++ flickr-call.class.php
@@ -13,6 +13,57 @@ Class Eli_Flickr_Call{
         $this->token_secret     = get_option( 'eli_token_secret', '' );
     }
 
+    private function flickr_api_call( $add_parameters, $transient = false, $url = 'https://api.flickr.com/services/rest' ){
+
+        $response = false;
+
+        if( $transient ){
+
+            $transient_name = 'eli_flickr_' . md5( serialize( $add_parameters ) );
+
+            $response = get_transient( $transient_name );
+        }
+
+
+        if( ! $response ){
+
+            $parameters = array_merge( array(
+                    'oauth_consumer_key'    => $this->consumer_key,
+                    'oauth_nonce'           => uniqid(),
+                    'oauth_signature_method'=> 'HMAC-SHA1',
+                    'oauth_token'           => $this->oauth_urlencode( $this->oauth_token ),
+                    'oauth_timestamp'       => time(),
+                    'oauth_version'         => '1.0',
+                    'nojsoncallback'        => '1',
+                    'format'                => 'json',
+                ), $add_parameters );
+
+            $signature = $this->signParams( $url, $parameters );
+
+            $parameters['oauth_signature'] = $this->oauth_urlencode( $signature );
+
+            $url_request_token = add_query_arg( $parameters, $url );
+
+            $wp_response = wp_remote_get( $url_request_token );
+
+            if( ! is_wp_error( $wp_response ) ){
+
+                $response_body = wp_remote_retrieve_body( $wp_response );
+
+                $response = json_decode( $response_body );
+
+                if( $transient ){
+
+                    set_transient( $transient_name, $response, HOUR_IN_SECONDS );
+                }
+
+            }
+        }
+
+        return $response;
+
+    }
+
     function flickr_request_token(){
 
         $request_token = false;
@@ -39,7 +90,7 @@ Class Eli_Flickr_Call{
         if( ! is_wp_error( $response ) ){
 
             $response_body = wp_remote_retrieve_body( $response );
-            error_log( print_r( $response_body, true ) );
+
             $request_token = $this->explode_query_args( $response_body );
 
         }
@@ -85,91 +136,26 @@ Class Eli_Flickr_Call{
 
     function flickr_get_my_album(){
 
-        $my_albums = get_transient( 'eli_flickr_get_my_album' );
+        $parameters = array( 'method' => 'flickr.photosets.getList' );
 
-        if( ! $my_albums ){
 
-            $url = 'https://api.flickr.com/services/rest';
-
-            $parameters = array(
-                    'oauth_consumer_key'    => $this->consumer_key,
-                    'oauth_nonce'           => uniqid(),
-                    'oauth_signature_method'=> 'HMAC-SHA1',
-                    'oauth_token'           => $this->oauth_urlencode( $this->oauth_token ),
-                    'oauth_timestamp'       => time(),
-                    'oauth_version'         => '1.0',
-                    'nojsoncallback'        => '1',
-                    'format'                => 'json',
-                    'method'                => 'flickr.photosets.getList',
-                );
-
-            $signature = $this->signParams( $url, $parameters );
-
-            $parameters['oauth_signature'] = $this->oauth_urlencode( $signature );
-
-            $url_request_token = add_query_arg( $parameters, $url );
-
-            $response = wp_remote_get( $url_request_token );
-
-            if( ! is_wp_error( $response ) ){
-
-                $response_body = wp_remote_retrieve_body( $response );
-
-                $my_albums = json_decode($response_body);
-
-                set_transient( 'eli_flickr_get_my_album', $my_albums, HOUR_IN_SECONDS );
-
-            }
-        }
-
-        return $my_albums;
+        return $this->flickr_api_call( $parameters, true );
 
     }
 
-    function flickr_get_album_photos( $photoset_id ){
+    function flickr_get_album_photos( $photoset_id, $page = 1, $per_page = 100 ){
 
-        $photos = get_transient( 'eli_flickr_get_album_photos_' . $photoset_id );
+        $parameters = array(
+                'method'                => 'flickr.photosets.getPhotos',
+                'photoset_id'           => $photoset_id,
+                'privacy_filter'        => 4,
+                'media'                 => 'photos',
+                'extras'                => 'date_taken,tags',
+                'per_page'              => $per_page,
+                'page'                  => $page,
+            );
 
-        if( ! $photos ){
-
-            $url = 'https://api.flickr.com/services/rest';
-
-            $parameters = array(
-                    'oauth_consumer_key'    => $this->consumer_key,
-                    'oauth_nonce'           => uniqid(),
-                    'oauth_signature_method'=> 'HMAC-SHA1',
-                    'oauth_token'           => $this->oauth_urlencode( $this->oauth_token ),
-                    'oauth_timestamp'       => time(),
-                    'oauth_version'         => '1.0',
-                    'nojsoncallback'        => '1',
-                    'format'                => 'json',
-                    'method'                => 'flickr.photosets.getPhotos',
-                    'photoset_id'           => $photoset_id,
-                    'privacy_filter'        => 4,
-                    //'per_page'              => 20,
-                    //'page'                  => 1,
-                );
-
-            $signature = $this->signParams( $url, $parameters );
-
-            $parameters['oauth_signature'] = $this->oauth_urlencode( $signature );
-
-            $url_request_token = add_query_arg( $parameters, $url );
-
-            $response = wp_remote_get( $url_request_token );
-
-            if( ! is_wp_error( $response ) ){
-
-                $response_body = wp_remote_retrieve_body( $response );
-
-                $photos = json_decode( $response_body );
-
-                set_transient( 'eli_flickr_get_album_photos_' . $photoset_id, $photos, HOUR_IN_SECONDS );
-
-            }
-        }
-
-        return $photos;
+        return $this->flickr_api_call( $parameters, true );
 
     }
 
